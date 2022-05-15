da70e270c8a62730d58d8afa6eab55c180a9c280
ali-osama-ali <85011048+ali-osama-ali@users.noreply.github.com>
2022-05-15 10:57:03 +0200

    Update find_network_holes.sh

--- find_network_holes.sh
+++ find_network_holes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo >/home/ali_osama/tcm_course/tmp.txt
-for i in {1..254}; do
-    ping 192.168.1.$i -c 1 -w 1 | grep "64 bytes" >>/home/ali_osama/tcm_course/tmp.txt
+for i in {1..10}; do
+    ping 192.168.1.$i -c 1 -w 1 | grep "64 bytes" | awk {'print $4'} | tr -d ":" >>/home/ali_osama/tcm_course/tmp.txt
 done
 
 for ip in $(cat tmp.txt); do
